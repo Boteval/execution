8b3f4975f7105538ee8375351db917c6b35d2847
matan <dev.matan@gmail.com>
2017-01-27 17:27:36 +0200

    get self git hash

--- src/runner/core.clj
+++ src/runner/core.clj
@@ -1,10 +1,16 @@
-(ns runner.core (:gen-class))
+(ns runner.core
+  (:require [clojure.java.shell :as shell])
+  (:require [clojure.string :as str]))
 
-(defn foo
-  "I don't do a whole lot."
-  [x]
-  (println x "Hello, World!"))
+(defn startup []
+  ; http://stackoverflow.com/a/29528037/1509695
+  ; http://stackoverflow.com/questions/38934681/how-to-get-timestamp-neutral-git-hash-from-a-given-commit-hash
+  ; http://dev.clojure.org/jira/browse/CLJ-124
+  (let [{git-hash :out exit-code :exit} (shell/sh "git" "describe" "--always" "--dirty")]
+    (if (= exit-code 0
+      (println (str "Starting (self git hash is " (str/trimr git-hash) ")")))
+      (println "cannot get self git hash"))))
 
-(defn -main
-  [x]
-  (foo x))
+(defn -main []
+  (startup)
+)
